d27ee1a47e5eca7bf66dff2840f8b1189cd0c4a0
nami-p <namipr3@gmail.com>
2024-05-09 01:24:41 +0300

    initial empty controllers and routes

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
 require('dotenv').config();
 
 require('./config/db');
+
+
+const app = express();
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+
+app.use(morgan("dev")); 
+// app.use(cors({origin:'http:// localhost:4200'})); // אפשור רק לכתובת מסוימת
+app.use(cors()); 
